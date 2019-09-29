c509bb9cbe8349ba87d596e39253f4a5eae88822
Eve Stockstill <evestockstillpdx@gmail.com>
2019-09-29 16:26:10 -0700

    final

---
+++ app.js
@@ -0,0 +1,28 @@
+function flip1() {
+    document.getElementById('t1').innerHTML = `min-height, min-width`;
+}
+function flip2() {
+    
+    document.getElementById('t2').innerHTML = `Properties that define parts of an element that takes up space on the page.
+    Content(height/width) Padding, Border, Margin`;
+
+}
+function flip3() {
+    document.getElementById('t3').innerHTML = `Width, style, color. Width can be in px, "thin", "medium", or "thick" Style examples include "none", "dotted"(circa 2002), and "solid." Color can chose what color it would be. The sytax looks like <br> p { <br>
+        border: 5px solid blue; <br>
+    }`;  
+}
+function flip4() {
+    document.getElementById('t4').innerHTML = `It is the space between the contents (height/width) and the border. You can be specific with padding-top: padding-right: padding-bottom: padding-left:<br>
+    shorthand padding: 2px; gives same border all around. padding: 2px 5px; gives a 2px border top/bottom and 5px left/right.`;
+}
+function flip5() {
+    document.getElementById('t5').innerHTML = `Fist you must include a WIDTH or it will take on the containing element like the body. <br> margin: 0 auto; will set the top and bottom to 0px while the auto will center according to your width container.`;
+}
+function flip6() {
+    document.getElementById('t6').innerHTML = `Using the display property <br>
+    li { <br>
+        display: inline;
+        <br>
+    }`; 
+}
